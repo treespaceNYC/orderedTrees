c61643050ae30426c60e178fa903aa4c95a3f6cf
Adrian-Mok15 <ADrian.Mok15@myhunter.cuny.edu>
2021-07-20 15:59:08 -0400

    Added some tests

--- interval_newick.py
+++ interval_newick.py
@@ -1,8 +1,15 @@
+things = [ [1,2], [1,3]  ]
+things = [[1, 3], [1, 4], [1, 5], [1, 6], [1, 9], [2, 3], [7, 9], [8, 9] ]
+# things = [[1, 2], [1, 3], [1, 4]]
+# things = [[1, 6], [2, 3], [2, 6], [4, 5], [4, 6]]
+d = { 1:[2,3] }
 def intervalNewick(interval):
     intervals = defaultdict(list)
     for k, v in interval:
         intervals[k].append(v)
     minMax = {}
+    # Using number of values of each key
+    # to find how nested it is
     for key, val in intervals.items():
         intervals[key] = sorted(intervals[key])
         for i in val:
@@ -18,3 +25,4 @@ def intervalNewick(interval):
         else:
             result+= str(key) + (val[0]*val[1]) + ","
     return result[:-1]
+print(intervalNewick(things))
