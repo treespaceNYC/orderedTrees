3f0fa70eff14c1f20c07f8295f7ccabe0927e2e0
tanhajyoti <56899043+tanhajyoti30@users.noreply.github.com>
2021-07-26 10:19:23 -0400

    Update randominterval.py

--- randominterval.py
+++ randominterval.py
@@ -1,14 +1,17 @@
 import random
 def randomInterval(min, max=None):
     
-    mid = 3
+    mid = random.randint(min, max-1)
     if (min+1==max):
      return None
     elif(min==mid):
         Interval=[mid+1,max]
         return Interval
+    elif(min+2==max):
+        Interval=[min,min+1]
+        return Interval
     else:
         Interval = [min,mid]
         interval2 = [mid+1,max]
         return Interval,interval2
-print (randomInterval(1,5))
+print (randomInterval(5,10))
